299628d8aaf10b8870b786ead1e35d82becdf825
Masahiro <masahiro.infinite77@gmail.com>
2018-12-13 01:34:21 +0900

    flattenメソッドの実装

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,11 @@
 object Main {
-  def flatten(list: List[_]): List[Any] = ???
+  def flatten(list: List[_]): List[Any] = 
+    list.isEmpty match {    
+      case true => Nil
+      case false =>
+        list.head match {
+          case a:List[_] => printList(a) ++ printList(list.tail)
+          case _ => list.head :: printList(list.tail)
+        }
+    }    
 }
