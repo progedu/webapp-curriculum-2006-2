cfdaaeb34a62c1484191502b01675ad293df6495
syo <M.youta.kwg@gmail.com>
2019-05-13 00:05:04 +0900

    flattenを実装

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,18 @@
+import scala.annotation.tailrec
+
 object Main {
-  def flatten(list: List[_]): List[Any] = ???
+  def flatten(list: List[_]): List[Any] = {
+    @tailrec
+    def loop(list: List[_], stack: List[List[_]], acc: List[Any]): List[Any] = list match {
+      case x::xs => x match {
+        case v: List[_] => loop(v, xs :: stack, acc)
+        case _ => loop(xs, stack, x::acc)
+      }
+      case Nil => stack match {
+        case x::xs => loop(x, xs, acc)
+        case Nil => acc
+      }
+    }
+    loop(list, Nil, Nil).reverse
+  }
 }
