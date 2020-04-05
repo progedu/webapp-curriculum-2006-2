4f2d4dbb95faa54283476620678e89b49398485e
QWYNG <ikusawasi@gmail.com>
2020-04-05 15:44:25 +0900

    impl my flatten

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,11 @@
 object Main {
-  def flatten(list: List[_]): List[Any] = ???
+  def flatten(list: List[_]): List[Any] = {
+    list
+      .foldLeft(Nil: List[Any])({ (a, b) =>
+        b match {
+          case list_element: List[_] => a ++ flatten(list_element)
+          case e                     => a ++ List(e)
+        }
+      })
+  }
 }
