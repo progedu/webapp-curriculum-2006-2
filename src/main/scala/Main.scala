d9fd8262c995c64feb3f4a26e8a25aaa131413cc
JojiKoike <joji.koike@gmail.com>
2020-06-13 00:17:29 +0900

    flattenの実装

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,7 @@
 object Main {
-  def flatten(list: List[_]): List[Any] = ???
+  def flatten(list: List[_]): List[Any] = list match {
+    case Nil                => Nil
+    case (x: List[_]) :: xs => flatten(x) :: flatten(xs)
+    case x :: xs            => x :: flatten(xs)
+  }
 }
