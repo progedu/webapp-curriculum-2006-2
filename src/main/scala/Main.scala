530f91c2029a04fc7ff05aa7090185febc1cff2c
hosh294 <132256088+hosh294@users.noreply.github.com>
2023-05-12 17:32:02 +0900

    flatten(List[の実装

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,7 @@
 object Main {
-  def flatten(list: List[_]): List[Any] = ???
+  def flatten(list: List[_]): List[Any] = list match {
+    case (h: List[_]) :: t => flatten(h) ++ flatten(t)
+    case h :: t => h :: flatten(t)
+    case Nil => Nil
+  }
 }
